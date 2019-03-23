9138e5d358373f42ff29cf794a37d8e6bcdb2885
Quan <toolowner@hotmail.com>
2019-03-23 15:31:05 +0700

    migrate on staging

--- .travis/deploy_staging.sh
+++ .travis/deploy_staging.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
-ssh $USER_STAGING_SERVER@$STAGING_SERVER -v exit
+ssh $USER_STAGING_SERVER@$STAGING_SERVER
+cd /var/www/healthonline/
+git pull
+composer install
+php artisan migrate:refresh --seed
