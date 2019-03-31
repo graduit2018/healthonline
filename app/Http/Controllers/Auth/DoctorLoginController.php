6d6cb7ce866f966b8dc8fc394fc0bda01478b172
Quan <toolowner@hotmail.com>
2019-03-31 16:53:46 +0700

    add missing library

--- app/Http/Controllers/Auth/DoctorLoginController.php
+++ app/Http/Controllers/Auth/DoctorLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class DoctorLoginController extends Controller
 {
