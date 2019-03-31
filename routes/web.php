569b7d06f685390e6a3152375b1fb409ece8dc1d
Quan <toolowner@hotmail.com>
2019-04-01 00:17:28 +0700

    logout route

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,20 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::prefix('admin')->group(function() {
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login');
+    Route::post('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
     Route::get('/dashboard', 'Admin\AdminController@index')->name('admin.dashboard');
 });
 
 Route::prefix('doctor')->group(function() {
     Route::get('/login', 'Auth\DoctorLoginController@showLoginForm')->name('doctor.login');
     Route::post('/login', 'Auth\DoctorLoginController@login');
+    Route::post('/logout', 'Auth\DoctorLoginController@logout')->name('doctor.logout');
     Route::get('/dashboard', 'Doctor\DoctorController@index')->name('doctor.dashboard');
 });
 
 Route::prefix('patient')->group(function() {
     Route::get('/login', 'Auth\PatientLoginController@showLoginForm')->name('patient.login');
     Route::post('/login', 'Auth\PatientLoginController@login');
+    Route::post('/logout', 'Auth\PatientLoginController@logout')->name('patient.logout');
     Route::get('/dashboard', 'Patient\PatientController@index')->name('patient.dashboard');
 });
