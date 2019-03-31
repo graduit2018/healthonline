e424e067c5175e69b9818a85e445e8f5d6763967
Quan <toolowner@hotmail.com>
2019-04-01 00:59:03 +0700

    change skin theme

--- resources/views/layouts/patient.blade.php
+++ resources/views/layouts/patient.blade.php
@@ -33,7 +33,7 @@
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition skin-red-light sidebar-mini">
     <!-- Site wrapper -->
     <div class="wrapper">
         @include('layouts.patient.partials.header')
