d401ff3849a6ec946f854d9c1128da078271d256
Quan <toolowner@hotmail.com>
2019-04-01 23:44:54 +0700

    composer dump-autoload

--- .travis/deploy_staging.sh
+++ .travis/deploy_staging.sh
@@ -2,5 +2,6 @@
 set -e
 ssh $USER_STAGING_SERVER@$STAGING_SERVER "cd /var/www/healthonline/; git pull;"
 ssh $USER_STAGING_SERVER@$STAGING_SERVER "cd /var/www/healthonline/; composer install;"
+ssh $USER_STAGING_SERVER@$STAGING_SERVER "cd /var/www/healthonline/; composer dump-autoload;"
 ssh $USER_STAGING_SERVER@$STAGING_SERVER "cd /var/www/healthonline/; php artisan migrate:refresh --seed;"
 
