e808aa1e4d1542c891b750fe22387d0f7e7ecc4d
Quan <toolowner@hotmail.com>
2019-04-11 16:03:37 +0700

    layout structure

--- resources/views/patient/auth/passwords/reset.blade.php
+++ resources/views/patient/auth/passwords/reset.blade.php
@@ -1,103 +1,53 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('patient.layouts.auth.layout')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Patient | Reset Password</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.4.8/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.10/css/AdminLTE.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/blue.css">
+@section('title')
+Patient | Reset Password
+@endsection
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+@section('content-header')
+<b>Patient</b>ResetPassword
+@endsection
 
-    <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
+@section('content')
+<form action="{{ route('patient.password.update') }}" method="post">
+    @csrf
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-            <a href="{{ url('/') }}"><b>Patient</b>ResetPassword</a>
-        </div>
-        <!-- /.login-logo -->
-        <div class="login-box-body">
+    <input type="hidden" name="token" value="{{ $token }}">
 
-            <form action="{{ route('patient.password.update') }}" method="post">
-                @csrf
-
-                <input type="hidden" name="token" value="{{ $token }}">
-
-                <div class="form-group has-feedback">
-                    <input id="email" type="email" placeholder="{{ __('E-Mail Address') }}" class="form-control" name="email" value="{{ old('email') }}"
-                        autofocus>
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-                </div>
-                @if ($errors->has('email'))
-                <span class="help-block text-red">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-                @endif
-
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="Password" name="password" required>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                @if ($errors->has('password'))
-                <span class="help-block text-red">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-                @endif
-
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="Confirm Password" name="password_confirmation" required>
-                    <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
-                </div>
-
-                <div class="row">
-                    <div class="col-xs-4">
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-xs-8">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">{{ __('Reset Password') }}</button>
-                    </div>
-                    <!-- /.col -->
-                </div>
-            </form>
-        </div>
-        <!-- /.login-box-body -->
+    <div class="form-group has-feedback">
+        <input id="email" type="email" placeholder="{{ __('E-Mail Address') }}" class="form-control" name="email" value="{{ old('email') }}"
+            autofocus>
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
     </div>
-    <!-- /.login-box -->
+    @if ($errors->has('email'))
+    <span class="help-block text-red">
+        <strong>{{ $errors->first('email') }}</strong>
+    </span>
+    @endif
 
-    <!-- jQuery 3 -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- Bootstrap 3.3.7 -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <!-- iCheck -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
-    <script>
-        $(function () {
-            $('input').iCheck({
-                checkboxClass: 'icheckbox_square-blue',
-                radioClass: 'iradio_square-blue',
-                increaseArea: '20%' /* optional */
-            });
-        });
-    </script>
-</body>
+    <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password" name="password" required>
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+    </div>
+    @if ($errors->has('password'))
+    <span class="help-block text-red">
+        <strong>{{ $errors->first('password') }}</strong>
+    </span>
+    @endif
 
-</html>
+    <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Confirm Password" name="password_confirmation" required>
+        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-4">
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-8">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">{{ __('Reset Password') }}</button>
+        </div>
+        <!-- /.col -->
+    </div>
+</form>
+@endsection
